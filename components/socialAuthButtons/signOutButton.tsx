3df354834d6a066813c806a136a3152c06a09291
RikkeMK <rikkemkodahl@gmail.com>
2026-04-22 16:46:30 +0200

    add Pressable + style

--- components/socialAuthButtons/signOutButton.tsx
+++ components/socialAuthButtons/signOutButton.tsx
@@ -1,7 +1,7 @@
 import { supabase } from "@/lib/supabase";
 import { useRouter } from "expo-router";
 import React from "react";
-import { Button } from "react-native";
+import { Pressable, StyleSheet, Text } from "react-native";
 
 export default function SignOutButton() {
   const router = useRouter();
@@ -17,5 +17,26 @@ export default function SignOutButton() {
     router.replace("/login");
   }
 
-  return <Button title="Sign out" onPress={onSignOutButtonPress} />;
+  return (
+    <Pressable onPress={onSignOutButtonPress} style={styles.button}>
+      <Text style={styles.buttonText}>Sign Out</Text>
+    </Pressable>
+  );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    padding: 10,
+    backgroundColor: "#F1F8E9",
+    borderColor: "#2E7D32",
+    borderWidth: 0.5,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "#2E7D32",
+    fontSize: 14,
+    fontFamily: "Nunito_400Regular",
+  },
+});
